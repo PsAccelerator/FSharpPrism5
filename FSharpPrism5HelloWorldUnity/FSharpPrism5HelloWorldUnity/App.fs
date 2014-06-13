fdca1ce7bb14ec4cef27a04da39d5bec51288969
amersfoort <p.amersfoort@psaccelerator.nl>
2014-06-13 11:05:09 +0200

    added prism and update files

--- FSharpPrism5HelloWorldUnity/FSharpPrism5HelloWorldUnity/App.fs
+++ FSharpPrism5HelloWorldUnity/FSharpPrism5HelloWorldUnity/App.fs
@@ -3,16 +3,32 @@
 open System
 open System.Windows
 open System.Windows.Controls
+open Microsoft.Practices.Prism.Modularity
+open Microsoft.Practices.Unity
+open Microsoft.Practices.Prism.UnityExtensions
+
 open FSharpx
 
-type MainWindow = XAML<"MainWindow.xaml">
+type Shell = XAML<"Shell.xaml">
 
-let loadWindow() =
-   let window = MainWindow()
-   
-   // Your awesome code goes here and you have strongly typed access to the XAML via "window"
-   
-   window.Root
+type App() =
+    inherit Application()
+        override x.OnStartup(e) =
+            base.OnStartup(e)
+            let bootstrapper = new DemoBootstrapper() 
+            bootstrapper.Run()
+
+and DemoBootstrapper() =
+    inherit UnityBootstrapper()
+    override x.CreateShell() = 
+        let window = Shell()
+        window.Root :> DependencyObject
+    override x.InitializeShell() =
+        base.InitializeShell()
+        App.Current.MainWindow <- x.Shell :?> Window
+        App.Current.MainWindow.Show()
+    override x.ConfigureModuleCatalog() = 
+        base.ConfigureModuleCatalog()
 
 [<STAThread>]
-(new Application()).Run(loadWindow()) |> ignore
+(new App()).Run()|> ignore
