a4f14501971a342543bcab3e9044de7aba3a66a4
amersfoort <p.amersfoort@psaccelerator.nl>
2014-06-13 11:36:04 +0200

    add apps file
    add missing apps file

---
+++ FSharpPrism5HelloWorldUnity/HelloWorldModule/HelloWorldModule/App.fs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorldModule
+
+open System
+open System.Windows
+open System.Windows.Controls
+
+open Microsoft.Practices.Prism.Modularity
+open Microsoft.Practices.Prism.Regions
+open Microsoft.Practices.Prism.Mvvm
+
+open FSharpx
+
+type HelloWorldXaml = XAML<"HelloWorldView.xaml">
+
+type HelloWorldModule(registry:IRegionViewRegistry) =
+    let mutable regionViewRegistry = registry
+    interface IModule with
+        member x.Initialize() = regionViewRegistry.RegisterViewWithRegion("MainRegion", fun _ -> HelloWorldXaml().Root:> obj)
