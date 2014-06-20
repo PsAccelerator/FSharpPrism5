5627fcc875e6c42b5f77f415b7107af70d308a92
amersfoort <p.amersfoort@psaccelerator.nl>
2014-06-20 15:35:39 +0200

    Add shell functionality
    Add shell functionality

--- FSharpPrism5MEF/Shell/App.fs
+++ FSharpPrism5MEF/Shell/App.fs
@@ -1,18 +1,46 @@
 ﻿module MainApp
 
 open System
+open System.IO
 open System.Windows
+open System.ComponentModel.Composition
+open System.ComponentModel.Composition.Hosting
 open System.Windows.Controls
+open Microsoft.Practices.Prism.Modularity
+open Microsoft.Practices.Prism.MefExtensions
 open FsXaml
 
-type MainWindow = XAML<"MainWindow.xaml">
+type Shell = XAML<"Shell.xaml">
 
-let loadWindow() =
-   let window = MainWindow()
-   
-   // Your awesome code goes here and you have strongly typed access to the XAML via "window"
-   
-   window.CreateRoot()
+type App() =
+    inherit Application()
+        override x.OnStartup(e) =
+            base.OnStartup(e)
+            let bootstrapper = new Bootstrapper() 
+            bootstrapper.Run()
+
+and Bootstrapper() =
+    inherit MefBootstrapper()
+        override x.CreateShell() = 
+            let window = Shell()
+            window.CreateRoot() :> DependencyObject
+        override x.InitializeShell() =
+            base.InitializeShell()
+            App.Current.MainWindow <- x.Shell :?> Window
+            App.Current.MainWindow.Show()
+        override x.ConfigureContainer() =
+             base.ConfigureContainer()
+        override x.CreateModuleCatalog() =
+             new ConfigurationModuleCatalog() :> IModuleCatalog
+        override x.ConfigureModuleCatalog() =
+            base.ConfigureModuleCatalog()
+        override x.ConfigureAggregateCatalog() =
+            x.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof<Bootstrapper>.Assembly))
+            let path = @"..\..\..\DirectoryModules"
+            let dir = new DirectoryInfo(path);
+            if not dir.Exists then dir.Create()
+            let catalog = new DirectoryCatalog(path)
+            x.AggregateCatalog.Catalogs.Add(catalog)
 
 [<STAThread>]
-(new Application()).Run(loadWindow()) |> ignore
+(new App()).Run()|> ignore
