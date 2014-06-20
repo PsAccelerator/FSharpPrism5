cd3dd107ec3409bee4e50aaa62e7b8ab245032bd
amersfoort <p.amersfoort@psaccelerator.nl>
2014-06-20 16:19:20 +0200

    added hello world module to mef
    added hello world module to mef

---
+++ FSharpPrism5MEF/HelloWorldModule/Apps.fs
@@ -0,0 +1,23 @@
+﻿namespace HelloWorldModule
+
+open System
+open System.Windows
+open System.Windows.Controls
+open System.ComponentModel.Composition
+
+open Microsoft.Practices.Prism.Modularity
+open Microsoft.Practices.Prism.Regions
+open Microsoft.Practices.Prism.Mvvm
+open Microsoft.Practices.Prism.MefExtensions.Modularity
+
+open FsXaml
+
+type HelloWorldXaml = XAML<"HelloWorldView.xaml">
+
+[<ModuleExport(typeof<HelloWorldModule>)>]
+type HelloWorldModule =
+    val regionViewRegistry: IRegionViewRegistry
+    interface IModule with
+        member x.Initialize() = x.regionViewRegistry.RegisterViewWithRegion("MainRegion", fun _ -> HelloWorldXaml():> obj)
+    [<ImportingConstructor>]
+    new (regionViewRegistry) = {regionViewRegistry = regionViewRegistry}
